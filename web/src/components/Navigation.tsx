d31819dc3a7183d3a72b79571c2139a8ab085ef8
Dev <Dev@HYPERV>
2025-11-02 14:50:47 +0800

    1

--- web/src/components/Navigation.tsx
+++ web/src/components/Navigation.tsx
@@ -84,9 +84,9 @@ const Navigation = observer((props: Props) => {
   return (
     <header className={cn("w-full h-full overflow-auto flex flex-col justify-between items-start gap-4 hide-scrollbar", className)}>
       <div className="w-full px-1 py-1 flex flex-col justify-start items-start space-y-2 overflow-auto overflow-x-hidden hide-scrollbar shrink">
-        <NavLink className="mb-3 cursor-default" to={Routes.ROOT}>
+        <div className="mb-3 cursor-default">
           <MemosLogo collapsed={collapsed} />
-        </NavLink>
+        </div>
         {navLinks.map((navLink) => (
           <NavLink
             className={({ isActive }) =>
