31a8cadf0dbc153dcb8e657a083d403a443c1843
Dev <Dev@HYPERV>
2025-11-02 14:57:30 +0800

    remove theme select

--- web/src/components/AuthFooter.tsx
+++ web/src/components/AuthFooter.tsx
@@ -12,7 +12,6 @@ const AuthFooter = observer(({ className }: Props) => {
   return (
     <div className={cn("mt-4 flex flex-row items-center justify-center w-full gap-2", className)}>
       <LocaleSelect value={workspaceStore.state.locale} onChange={(locale) => workspaceStore.state.setPartial({ locale })} />
-      <ThemeSelect value={workspaceStore.state.theme} onValueChange={(theme) => workspaceStore.state.setPartial({ theme })} />
     </div>
   );
 });
